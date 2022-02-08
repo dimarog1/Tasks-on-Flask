6ed0e9ff4fe2977759aea96a425525817ee3d03e
dimarog1 <76162370+dimarog1@users.noreply.github.com>
2022-02-08 18:04:24 +0300

    задание 6

--- server.py
+++ server.py
@@ -169,5 +169,37 @@ def astronaut_selection():
         return "Форма отправлена"
 
 
+@app.route('/choice/<planet>')
+def choice(planet):
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                   <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>{planet}</title>
+                  </head>
+                  <body>
+                    <h1>Моё предложение: {planet}</h1>
+                    <b>Эта планета близка к земле;</b>
+                    <div class="alert alert-success" role="alert">
+                      <b>На ней много необходимых ресурсов;</b>
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                      <b>На ней есть вода и атмосфера;</b>
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                     <b> На ней есть небольшое магнитное поле;</b>
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                     <b>Накоенц, она просто красива!;</b>
+                    </div>
+                  </body>
+                </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
